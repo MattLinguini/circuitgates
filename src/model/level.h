9bc8cafcc560a7fcffc2b8e4b8e224b72247c0ec
Alex Johnson <a.xohnson@gmail.com>
2025-04-11 14:33:08 -0600

    Added vector of GameObject pointers to level.

--- src/model/level.h
+++ src/model/level.h
@@ -2,6 +2,8 @@
 #define LEVEL_H
 
 #include <QJsonObject>
+#include <vector>
+#include "game_object.h"
 
 class Level
 {
@@ -11,6 +13,9 @@ public:
     /// @brief Generates a json representation of this object.
     /// @return A QJsonObject containing this level.
     QJsonObject toJson();
+
+    /// @brief Contains all the GameObjects within a level.
+    std::vector<GameObject*> gameObjs;
 };
 
 #endif // LEVEL_H
