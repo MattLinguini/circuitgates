5f8f6661987820e27e88de7a1167c4a0553daaff
arkellmer <arkellmer@hotmail.com>
2025-04-23 13:26:51 -0600

    Updated tutorial text.

--- src/view/circuit_game_view.cpp
+++ src/view/circuit_game_view.cpp
@@ -271,13 +271,13 @@ void CircuitGameView::setupTutorialPage() {
                                 "In the center of the puzzle is a gate, with an input above and an output below.\n\n"
                                 "Inputs and outputs send and recieve their state as indicated by wires and their color.\n\n"
                                 "Your objective is to solve each puzzle such that the outputs match their expected state, which can be seen in the upper right corner of each output.\n\n"
-                                "You can slot the gates into a socket by dragging them into the socket.\n\n"
+                                "Click the inputs at the top to change their signal. You can slot the gates into a socket by dragging them into the socket.\n\n"
                                 "The top gate is an OR gate. This gate passes on positive state if one or either input is positive.\n\n"
                                 "The 2nd gate is an AND gate. This gate only passes positive state if both inputs are positive.\n\n"
                                 "The 3rd gate is a NOT gate. This gate passes on the opposite signal of its input.\n\n"
                                 "The last gate is an XOR gate. This gate only passes on state if some, but not all, inputs are positive.\n\n");
     tutorialText->setReadOnly(true);
-    tutorialText->setMaximumWidth(275);
+    tutorialText->setMaximumWidth(300);
 
     QPushButton *levelButton = new QPushButton("Return to Levels");
     connect(levelButton, &QPushButton::clicked, this, &CircuitGameView::displayLevels);
